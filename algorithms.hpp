aa6f3545d0fc3df06a84fe26e0660432d984823a
nnrogers515 <nnrogers515@gmail.com>
2020-11-25 15:15:10 -0600

    removed templating (commented it out incase we wanna revise later?)

--- algorithms.hpp
+++ algorithms.hpp
@@ -6,20 +6,19 @@
 /* Might not be easily doable without explicity using Vertex and such anyways! */
 
 namespace Alg {
-    template <class G, class V>
-    void shortest_path(G path, V start, V end) {
+    void shortest_path(Graph path, Graph::Vertex start, Graph::Vertex end) {
         //https://www.geeksforgeeks.org/c-program-for-dijkstras-shortest-path-algorithm-greedy-algo-7/
         std::cout << "\nShortest_Path:\n";
     }
 
-    template <class G, class V>
-    void bfs(G graph, V start) {
+    // template <class G, class V>
+    void bfs(Graph graph, Graph::Vertex start) {
         //https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
         std::cout << "\nBFS\n";
     }
 
-    template <class G>
-    void pagerank(G graph,  double alpha, int iterations, double tolerance) {
+    // template <class G>
+    void pagerank(Graph graph,  double alpha, int iterations, double tolerance) {
         //https://www.geeksforgeeks.org/page-rank-algorithm-implementation/
         std::cout << "\nPage_rank\n";
     }
