2e9b7c5beb05a8cddf1006a79fa4969c0e30889f
Mat Farley <mathewf2@linux-a3.ews.illinois.edu>
2020-12-03 20:20:25 -0600

    Rebase constructor into adjacency_matrix branch

--- main.cpp
+++ main.cpp
@@ -5,17 +5,15 @@
 int main(int argc, char *argv[]) {
     // std::cout << "I compile!" << std::endl;
     // int graph[10][10];
-    // int start = 0;
-    // int end = 1;
-    // Graph graph;
+    Graph graph;
     
-    // Vertex start;
-    // Vertex end;
+    Vertex start;
+    Vertex end;
 
-    // Alg::shortest_path(graph, start, end);
-    // Alg::bfs(graph, start);
-    // Alg::pagerank(graph, 0.5, 100, 0.000005);
-    // std::cout << "\n";
+    Alg::shortest_path(graph, start, end);
+    Alg::bfs(graph, start);
+    Alg::pagerank(graph, 0.5, 100, 0.000005);
+    std::cout << "\n";
 
     std::ifstream file("decoded_links.txt", std::ifstream::in);    
     Graph Graph(file);
