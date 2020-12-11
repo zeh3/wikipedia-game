0cda68fb3e4107818a1efb06c3d4b7f2a46ea7fd
Ahmed Hasan <aehasan2@linux-a1.ews.illinois.edu>
2020-12-11 11:55:12 -0600

    added create basic disconnected

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -36,6 +36,39 @@ Graph createSimpleGraph() {
     return graph;
 }
 
+Graph createBasicDisconnectedGraph() {
+    Graph graph;
+
+    graph.insertVertex("A");
+    graph.insertVertex("B");
+    graph.insertVertex("C");
+    graph.insertVertex("D");
+    graph.insertVertex("E");
+    graph.insertVertex("F");
+    graph.insertVertex("G");
+    graph.insertVertex("H");
+    graph.insertVertex("I");
+
+    graph.insertEdge("A","B");
+    graph.insertEdge("A","C");
+    graph.insertEdge("B","C");
+    graph.insertEdge("B","D");
+
+
+    graph.insertEdge("E","F");
+    graph.insertEdge("F","G");
+    graph.insertEdge("G","H");
+    graph.createAdjMat();
+
+
+
+    return graph;
+
+
+
+
+}
+
 // for checking adjacency matrix things
 int transformCoordinates(int x, int y, int size) {
     return size * y + x;
